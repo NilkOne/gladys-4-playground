503a27a48278116b9b2d5ad5f50ec57f4a0776e2
NilkOne <kevin.klein.pro@gmail.com>
2019-06-17 23:41:52 +0200

    edit Sonos getDevices service

--- server/services/sonos/lib/music/music.getDevices.js
+++ server/services/sonos/lib/music/music.getDevices.js
@@ -1,6 +1,5 @@
-
 const logger = require('../../../../utils/logger');
-
+const events = require('events');
 
 /**
  * @private
@@ -12,19 +11,30 @@ function getDevices() {
     if (!this.sonosApi) {
         throw new Error(`Sonos API not initialized`);
     }
+    logger.info('SonosService.getDevices');
 
     const DeviceDiscovery = this.sonosApi.DeviceDiscovery;
     
-    DeviceDiscovery({ timeout: 10000 }, (device) => {
-        const ipAddress = device.host;
-        logger.log('SonosService: Found device at {0}', ipAddress);
-    });
-    
     // TODO : use real data instead mock
-    const devices = [{name:'Living Room', ip:'192.168.1.98'}, {name:'Bath Room', ip:'192.168.1.99'}];
-    
-    logger.info(`SonosService: Found ${devices.length} devices`);
-    
+    // const devices = [{name:'Living Room', ip:'192.168.1.98'}, {name:'Bath Room', ip:'192.168.1.99'}];
+    let devices = [];
+
+    const addDevice = function addDevice(device) {
+        logger.log('SonosService: Found device at {0}:{1}', device.host, device.port);
+        devices.push({name:device.port, ip:device.host});
+    }
+
+    const returnDevices = function returnDevices() {
+        logger.info(`SonosService: Found ${devices.length} devices`);
+        return devices;
+    }
+
+    const eventEmitter = new events.EventEmitter();
+    eventEmitter.on('DeviceAvailable', addDevice);
+    eventEmitter.on('timeout', returnDevices);
+
+    DeviceDiscovery({ timeout: 10000 }, addDevice);
+
     return devices;
 }
 
